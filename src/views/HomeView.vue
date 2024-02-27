1d2d944c131b14389b359218eb42af03ce678cf7
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-02-27 10:08:00 +0530

    base added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>{{ apiData }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      apiData: [],
+    };
+  },
+  async mounted() {
+    this.apiData = await axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => (this.info = response));
+  },
+};
+</script>
